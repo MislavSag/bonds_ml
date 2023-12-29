05bf9ca68af277b0344e15230cb7e74798164beb
unknown <mislav.sagovac@contentio.biz>
2023-12-29 09:41:09 +0100

    add run job file and set sh for run jobs

--- jobs.sh
+++ jobs.sh
@@ -1,13 +1,11 @@
-
 #!/bin/bash
 
-#PBS -N PEAD
-#PBS -l ncpus=4
-#PBS -l mem=8GB
-#PBS -J 1-3652
+#PBS -N BondsJobs
+#PBS -l ncpus=1
+#PBS -l mem=2GB
+#PBS -J 1-10000
 #PBS -o experiments/logs
 #PBS -j oe
 
 cd ${PBS_O_WORKDIR}
 apptainer run image.sif run_job.R 0
-
