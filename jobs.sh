68139d7f92dd53dee0af5419e7966d851b861e0c
MislavSag <mislav.sagovac@contentio.biz>
2024-02-16 22:26:16 +0100

    decrase arrays

--- jobs.sh
+++ jobs.sh
@@ -1,14 +1,13 @@
-
 #!/bin/bash
 
 #PBS -N BondsML
 #PBS -l ncpus=4
 #PBS -l mem=4GB
-#PBS -J 1-10000
+#PBS -J 1-2804
 #PBS -o experiments/logs
 #PBS -j oe
 
 cd ${PBS_O_WORKDIR}
-apptainer run image.sif run_job.R 0
+apptainer run image.sif run_job.R 1
 
 # 12804
