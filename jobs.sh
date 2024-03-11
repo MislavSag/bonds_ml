13725fc88be4c1cc6424a11b97b22ce4ffddf6e5
MislavSag <mislav.sagovac@contentio.biz>
2024-03-11 18:14:31 +0100

    start

--- jobs.sh
+++ jobs.sh
@@ -3,11 +3,11 @@
 #PBS -N BondsML
 #PBS -l ncpus=4
 #PBS -l mem=4GB
-#PBS -J 1-300
+#PBS -J 1-10000
 #PBS -o experiments/logs
 #PBS -j oe
 
 cd ${PBS_O_WORKDIR}
-apptainer run image.sif run_job.R 1
+apptainer run image.sif run_job.R 0
 
 # 12804
