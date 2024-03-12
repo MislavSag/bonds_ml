d009c955626d64fc50e56b167e97fd377926eb35
MislavSag <mislav.sagovac@contentio.biz>
2024-03-12 20:05:47 +0100

    residue

--- jobs.sh
+++ jobs.sh
@@ -3,11 +3,12 @@
 #PBS -N BondsML
 #PBS -l ncpus=4
 #PBS -l mem=4GB
-#PBS -J 1-10000
+#PBS -J 1-6170
 #PBS -o experiments/logs
 #PBS -j oe
 
 cd ${PBS_O_WORKDIR}
-apptainer run image.sif run_job.R 0
-
-# 12804
+apptainer run image.sif run_job.R 1
+`
+#  16170
+# 10000
