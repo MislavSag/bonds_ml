56ef8150473e687c571d8a6e3c88a156a6fd00ad
MislavSag <mislav.sagovac@contentio.biz>
2024-05-06 20:14:11 +0200

    sh file

--- jobs.sh
+++ jobs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#PBS -N ZSEML
+#PBS -N GoldsML
 #PBS -l ncpus=4
 #PBS -l mem=6GB
-#PBS -J 10001-13464
+#PBS -J 1-1000
 #PBS -o experiments/logs
 #PBS -j oe
 
 cd ${PBS_O_WORKDIR}
 apptainer run image.sif run_job.R 0
 
-# 13464
+# 1228
